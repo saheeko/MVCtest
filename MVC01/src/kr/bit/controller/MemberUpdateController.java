1dbffbd386208728ccf28ffed3df668bbe8a502c
ksh89 <ksh89@SAHEE>
2024-02-07 15:56:08 +0900

    mvc(servlet/controller)로만 하는 과정(회원관리)

--- MVC01/src/kr/bit/controller/MemberUpdateController.java
+++ MVC01/src/kr/bit/controller/MemberUpdateController.java
@@ -1,4 +1,4 @@
- package kr.bit.controller;
+package kr.bit.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,38 +13,25 @@ import kr.bit.model.MemberDAO;
 import kr.bit.model.MemberVO;
 
 
-@WebServlet("/memberInsert.do")
-public class MemberInsertController extends HttpServlet {
+@WebServlet("/memberUpdate.do")
+public class MemberUpdateController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//클라이언트로부터 넘어온 파라메터 중에 한글데이터가 있을경우 한글이 꺠지지않게 하기위한것.
 		request.setCharacterEncoding("utf-8");
-		
-		//1.파라메터수집(vo) 개별로 넘어온 데이터를 받는 곳
-		String id=request.getParameter("id");
-		String pass=request.getParameter("pass");
-		String name=request.getParameter("name");
+		//파라메터 수집(vo)
+		int num=Integer.parseInt(request.getParameter("num"));
 		int age=Integer.parseInt(request.getParameter("age"));
 		String email=request.getParameter("email");
 		String phone=request.getParameter("phone");
 		
-		//파라메터 수집을 하기위해 VO로 작성 된것을 객체 생성하여 묶는곳 / 이곳이 파라미터 수집한곳
-		/*
-		 * MemberVO vo=new MemberVO(id, pass, name, age, email, phone);
-		 */
-		//이방법은 set으로 파라메터 수집을 하는 방법(기본방법)
-		 MemberVO vo=new MemberVO(); 
-		 vo.setId(id); 
-		 vo.setPass(pass); 
-		 vo.setName(name);
-		 vo.setAge(age); 
-		 vo.setEmail(email); 
-		 vo.setPhone(phone);
-		 
-		//System.out.println(vo);//vo.toString()이것이 호출되는거 (확인차)
-		 //model과 연동부분
-		 MemberDAO dao=new MemberDAO();
-		 int cnt=dao.memberInsert(vo);
-		 PrintWriter out=response.getWriter();
+		MemberVO vo=new MemberVO();
+		vo.setNum(num);
+		vo.setAge(age);
+		vo.setEmail(email);
+		vo.setPhone(phone);
+		
+		MemberDAO dao=new MemberDAO();
+		int cnt=dao.memberUpdate(vo);
 		 if(cnt>0) {
 			 //가입성공
 			 //out.println("insert success"); 다시 리스트페이지로가기위해 /MVC01/memberList.do
--- MVC01/src/kr/bit/db/member.sql
+++ MVC01/src/kr/bit/db/member.sql
@@ -7,8 +7,9 @@ pass varchar(20) not null,
 name varchar(30) not null,
 age int not null,
 email varchar(30) not null,
-phone varchar(30) not null
-)
+phone varchar(30) not null,
+unique key(id)
+);
 --SQL(CRUD), JDBC
 --검색
 select * from member;
@@ -22,3 +23,6 @@ update member set age=45, phone='010-1234-1234' where id='admin';
 
 --delete(삭제)
 delete from member where id='admin';
+
+--table삭제
+drop table member;
